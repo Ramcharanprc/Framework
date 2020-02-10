67ed7c820e53522181d4711925001951b2af56a1
Ramcharanprc <60866588+Ramcharanprc@users.noreply.github.com>
2020-02-10 17:25:26 +0530

    Update Module

--- UpdateModule.py
+++ UpdateModule.py
@@ -7,5 +7,4 @@ def update():
 	connection.execute("update " + tableName + " set " + columnNames[userOption] + " = '" + input("Enter " + fieldNames[userOption] + ": ") + " where " + columnName[0] + " = '" + userInput + "';")
 	connection.commit()
 	print(getGeneralMessage('Update'))
-	print()
 
