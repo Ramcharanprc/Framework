13d01f8cc9ff30ced76b4f620b357a3e91f27e6c
Sindhu45 <sindhugumidelli45@gmail.com>
2020-02-10 13:51:30 +0530

    Final version of create module.

--- Create.py
+++ Create.py
@@ -1,24 +1,14 @@
-import sqlite3
-active = "A"
-inactive = "D"
-database = "FrameWork.db"
-menuFile = "Menu.cfg"
-generalTable = "General"
-menuList = []
-fieldList = []
-connection = sqlite3.connect(database)
-
 def create():
 	fieldValues = []
-	for index in range(Zero, len(fieldList) - 1):
-		userInput = input("Enter " + fieldList[index] +": ")
+	for index in range(0, len(fieldNames) - 1):
+		userInput = input("Enter " + fieldNames[index] +": ")
 		if(index == 0):
 			fieldValues.append(int(userInput))
 		else:	
 			fieldValues.append(userInput)
-	fieldValues.append(activeStatus)		
+	fieldValues.append(active)
 	fieldValueTuple = tuple(fieldValues)
 	insertQuery = "insert into " + (tableName) + " values" + str(fieldValueTuple)
 	connection.execute(insertQuery)
 	connection.commit()
-	printGeneralMessage("Create")
+	print(printGeneralMessage("Create"))
