ab86693511e6175962bdb01d80afb312d84e279b
Syfali <shaiksyfali1819@gmail.com>
2020-02-10 17:16:51 +0530

    UpdateModule.py

--- UpdateModule.py
+++ UpdateModule.py
@@ -4,7 +4,7 @@ def update():
 	for updateCounter in range(1, len(fieldNames)):
 		print(str(updateCounter) + ". " + fieldNames[updateCounter])
 	userOption = input(getGeneralMessage('Update_prompt'))
-	connection.execute("update " + tableName + " set " + columnNames[userOption] +" = '" + input("Enter "+ fieldNames[userOption] + ": ") + " where " + columnName[0] +" = '" + userInput + "';")
+	connection.execute("update " + tableName + " set " + columnNames[userOption] + " = '" + input("Enter " + fieldNames[userOption] + ": ") + " where " + columnName[0] + " = '" + userInput + "';")
 	connection.commit()
 	print(getGeneralMessage('Update'))
 	print()
