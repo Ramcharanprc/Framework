c8422eb08c76b77bc4da4390fd26b1582a74f19d
Ramcharanprc <ramcharanprc@gmail.com>
2020-02-10 10:27:29 +0000

    Modified fields

--- FrameworkMain.py
+++ FrameworkMain.py
@@ -2,3 +2,10 @@
 import sqlite3
 
 databaseName = 'Framework.db'
+generalTable = 'generalConfigTable'
+connection = sqlite3.connect(databaseName)
+generalConfigurations = connection.execute("select * from ")
+fieldTable = connection.execute('.pragma table_info ' + general)
+fieldNames = []
+for row in fieldTable:
+	
