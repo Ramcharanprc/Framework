341a034af4ad55ce80b448e2f9b0c9d6a338d1c4
Syfali <shaiksyfali1819@gmail.com>
2020-02-10 16:06:32 +0530

    Update.py

--- Update.py
+++ Update.py
@@ -3,8 +3,8 @@ def update():
 	printRecord(getRecord())
 	for updateCounter in range(1, len(fieldNames)):
 		print(str(updateCounter) + ". " + fieldNames[updateCounter])
-	userOption = input(generalMessage('Update_prompt'))
-	connection.execute("update " + tableName + " set " + columnNames[userOption] +" = '" + input("Enter "+ fieldNames[userOption] + "' where " + columnName[0] +" = '" + userInput + "';")
+	userOption = input(getGeneralMessage('Update_prompt'))
+	connection.execute("update " + tableName + " set " + columnNames[userOption] +" = '" + input("Enter "+ fieldNames[userOption] + ": ") + " where " + columnName[0] +" = '" + userInput + "';")
 	connection.commit()
-	print(generalMessage('Update'))
+	print(getGeneralMessage('Update'))
 
